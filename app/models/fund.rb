5bc224eb87e2b8a83db78246062cba8298739ca6
Andrew Nesbitt <andrewnez@gmail.com>
2024-09-23 10:21:31 +0100

    Remove logging

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -60,7 +60,6 @@ class Fund < ApplicationRecord
   
       packages = packages.reject { |p| p['status'].present? || p['repository_url'].blank? }
       packages.each do |package|
-        puts package['repository_url']
         project = Project.find_or_create_by(url: package['repository_url'])
         project.repository = package['repo_metadata'] if project.repository.blank?
         project.packages += [package] unless project.packages.map{|pkg| pkg['registry_url']}.include?(package['registry_url'])
@@ -86,7 +85,6 @@ class Fund < ApplicationRecord
 
       packages = packages.reject { |p| p['repository_url'].blank? }
       packages.each do |package|
-        puts package['repository_url']
         project = Project.find_or_create_by(url: package['repository_url'])
         project.repository = package['repo_metadata'] if project.repository.blank?
         project.packages += [package] unless project.packages.map{|pkg| pkg['registry_url']}.include?(package['registry_url'])
