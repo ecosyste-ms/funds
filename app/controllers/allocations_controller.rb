50dec49a08220d808d758d9dd0f1ab15886d594b
Andrew Nesbitt <andrewnez@gmail.com>
2024-09-23 15:06:31 +0100

    eager loading

--- app/controllers/allocations_controller.rb
+++ app/controllers/allocations_controller.rb
@@ -2,6 +2,6 @@ class AllocationsController < ApplicationController
   def show
     @fund = Fund.find_by(slug: params[:fund_id])
     @allocation = @fund.allocations.find(params[:id])
-    @project_allocations = @allocation.project_allocations.includes(:project).order('amount_cents desc').where('amount_cents >= 1')
+    @project_allocations = @allocation.project_allocations.includes(project: :funding_source).order('amount_cents desc').where('amount_cents >= 1')
   end
 end
