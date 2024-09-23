1a7f33ddc3bd8d9b4ce0de6ba60518bd36fb62bf
Andrew Nesbitt <andrewnez@gmail.com>
2024-09-23 09:30:20 +0100

    Fix projects#show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 module ApplicationHelper
   include Pagy::Frontend
+  
+  def obfustcate_email(email)
+    return unless email.present?
+    
+    email.split('@').map do |part|
+      if part.length > 2
+        part.tap { |p| p[1...-1] = "****" }
+      else
+        part
+      end
+    end.join('@')
+  end
 end
