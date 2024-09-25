5bb4fe64151158554a144a9d80a89ef9aa1a5e7d
Andrew Nesbitt <andrewnez@gmail.com>
2024-09-25 16:06:02 +0100

    Fix tests

--- test/models/allocation_test.rb
+++ test/models/allocation_test.rb
@@ -4,17 +4,17 @@ class AllocationTest < ActiveSupport::TestCase
   test 'calculate_funded_projects' do
     fund = Fund.create!(name: 'Test Fund', slug: 'test', 'registry_name': 'npm')
 
-    project1 = Project.create!(url: 'https://github.com/base62/base62.js', registry_names: ['npm'], repository: { 'archived' => false })
+    project1 = Project.create!(url: 'https://github.com/base62/base62.js', licenses: ['mit'], registry_names: ['npm'], repository: { 'archived' => false })
     project1.stubs(:downloads).returns(100)
     project1.stubs(:dependent_repos).returns(100)
     project1.stubs(:dependent_packages).returns(100)
     
-    project2 = Project.create!(url: 'https://github.com/reduxjs/redux', registry_names: ['npm'], repository: { 'archived' => false })
+    project2 = Project.create!(url: 'https://github.com/reduxjs/redux', licenses: ['mit'], registry_names: ['npm'], repository: { 'archived' => false })
     project2.stubs(:downloads).returns(100)
     project2.stubs(:dependent_repos).returns(100)
     project2.stubs(:dependent_packages).returns(100)
 
-    project3 = Project.create!(url: 'https://github.com/expressjs/express', registry_names: ['npm'], repository: { 'archived' => false })
+    project3 = Project.create!(url: 'https://github.com/expressjs/express', licenses: ['mit'], registry_names: ['npm'], repository: { 'archived' => false })
     project3.stubs(:downloads).returns(100)
     project3.stubs(:dependent_repos).returns(100)
     project3.stubs(:dependent_packages).returns(100)
--- test/models/fund_test.rb
+++ test/models/fund_test.rb
@@ -4,8 +4,9 @@ class FundTest < ActiveSupport::TestCase
   test 'possible_projects' do
     fund = Fund.create!(name: 'Test Fund', slug: 'test', 'registry_name': 'npm')
 
-    project1 = Project.create!(url: 'https://github.com/base62/base62.js', registry_names: ['npm'], repository: { 'archived' => false })
+    project1 = Project.create!(url: 'https://github.com/base62/base62.js', licenses: ['mit'], registry_names: ['npm'], repository: { 'archived' => false })
     project2 = Project.create!(url: 'https://github.com/base62/archived', registry_names: ['npm'], repository: { 'archived' => true })
+    project2 = Project.create!(url: 'https://github.com/base62/nolicense', registry_names: ['npm'], repository: { 'archived' => false })
 
     assert_equal 1, fund.possible_projects.count
   end
