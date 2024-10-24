10f63bc733ae6c72901fe267522224c0ef920b07
Andrew Nesbitt <andrewnez@gmail.com>
2024-10-24 15:25:35 +0100

    Add associations and html_url method to Invitation model

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,2 +1,10 @@
 class Invitation < ApplicationRecord
+  belongs_to :project_allocation
+  belongs_to :fund, through: :project_allocation
+  belongs_to :project, through: :project_allocation
+  belongs_to :allocation, through: :project_allocation
+
+  def html_url
+    "https://staging.opencollective.com/#{ENV['OPENCOLLECTIVE_PARENT_SLUG']}/projects/#{fund.oc_project_slug}/expenses/#{member_invitation_id}"
+  end
 end
