164bb697d21ccddd518558d1c6a4f36d4295733c
Andrew Nesbitt <andrewnez@gmail.com>
2024-10-24 15:31:02 +0100

    Fix invitation class

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,10 +1,7 @@
 class Invitation < ApplicationRecord
   belongs_to :project_allocation
-  belongs_to :fund, through: :project_allocation
-  belongs_to :project, through: :project_allocation
-  belongs_to :allocation, through: :project_allocation
 
   def html_url
-    "https://staging.opencollective.com/#{ENV['OPENCOLLECTIVE_PARENT_SLUG']}/projects/#{fund.oc_project_slug}/expenses/#{member_invitation_id}"
+    "https://staging.opencollective.com/#{ENV['OPENCOLLECTIVE_PARENT_SLUG']}/projects/#{project_allocation.fund.oc_project_slug}/expenses/#{member_invitation_id}"
   end
 end
