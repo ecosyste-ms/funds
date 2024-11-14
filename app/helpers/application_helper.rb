e084a4d3f9eccc75821403afe217eccca96d8717
Andrew Nesbitt <andrewnez@gmail.com>
2024-11-14 13:57:01 +0000

    Display maintainer email obfusticated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,11 @@ module ApplicationHelper
   def meta_description
     @meta_description || 'An open source funding solution from ecosyste.ms & Open Source Collective.'
   end
+
+  def obfuscate_email(email)
+    return unless email.present?
+    local_part, domain = email.split('@')
+    obfuscated_local = local_part[0] + '*' * (local_part.length - 2) + local_part[-1]
+    "#{obfuscated_local}@#{domain}"
+  end
 end
