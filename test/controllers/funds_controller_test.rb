f9cde56681e010927fae024f09e49d66377a7951
Andrew Nesbitt <andrewnez@gmail.com>
2024-11-19 16:25:48 +0000

    Fixed test

--- test/controllers/funds_controller_test.rb
+++ test/controllers/funds_controller_test.rb
@@ -8,6 +8,7 @@ class FundsControllerTest < ActionDispatch::IntegrationTest
 
   test "should get show" do
     fund = Fund.create!(name: 'Test Fund', slug: 'test', 'registry_name': 'npm')
+    Allocation.create!(fund: fund, total_cents: 10_000_00)
     get fund_url(fund)
     assert_response :success
   end
