ef5d3bc5fbcefd66dc6dc899d222a04ef2991f91
Andrew Nesbitt <andrewnez@gmail.com>
2024-11-19 17:59:33 +0000

    Add support for thanks.dev funding source in Project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -728,6 +728,8 @@ class Project < ApplicationRecord
         "https://polar.sh/#{v}"
       when 'buy_me_a_coffee'
         "https://buymeacoffee.com/#{v}"
+      when 'thanks_dev'
+        "https://thanks.dev/#{v}"
       else
         v
       end
@@ -746,7 +748,7 @@ class Project < ApplicationRecord
   end
 
   def funding_domains
-    ['opencollective.com', 'ko-fi.com', 'liberapay.com', 'patreon.com', 'otechie.com', 'issuehunt.io', 
+    ['opencollective.com', 'ko-fi.com', 'liberapay.com', 'patreon.com', 'otechie.com', 'issuehunt.io', 'thanks.dev',
     'communitybridge.org', 'tidelift.com', 'buymeacoffee.com', 'paypal.com', 'paypal.me','givebutter.com', 'polar.sh']
   end
 
