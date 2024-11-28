eef909364573fd9f10103f90d043dfc7baa77806
Andrew Nesbitt <andrewnez@gmail.com>
2024-11-28 15:14:51 +0000

    Implement invitation show view and remove old accept/reject views

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,13 +1,18 @@
 class InvitationsController < ApplicationController
+
+  def show
+    @invitation = Invitation.find_by_token(params[:token])
+  end
+
   def accept
     # find invitation by token
     # invitation.accept
-    # redirect to back to self with a flash message
+    # redirect to invitation.html_url
   end
 
   def reject
     # find invitation by token
     # invitation.reject
-    # redirect back to self with a flash message
+    # redirect back invitation.html_url
   end
 end
