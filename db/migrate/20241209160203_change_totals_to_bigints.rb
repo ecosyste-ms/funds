c1ad123b0010eb33a3a68150c785abf184a81a60
Andrew Nesbitt <andrewnez@gmail.com>
2024-12-09 16:26:14 +0000

    Refactor project totals columns to use bigint for improved capacity and default values

--- db/migrate/20241209160203_change_totals_to_bigints.rb
+++ db/migrate/20241209160203_change_totals_to_bigints.rb
@@ -1,7 +1,12 @@
 class ChangeTotalsToBigints < ActiveRecord::Migration[8.0]
   def change
-    change_column :projects, :total_downloads, :bigint
-    change_column :projects, :total_dependent_repos, :bigint
-    change_column :projects, :total_dependent_packages, :bigint
+    remove_column :projects, :total_downloads, :integer
+    add_column :projects, :total_downloads, :bigint, default: 0
+
+    remove_column :projects, :total_dependent_repos, :integer
+    add_column :projects, :total_dependent_repos, :bigint, default: 0
+
+    remove_column :projects, :total_dependent_packages, :integer
+    add_column :projects, :total_dependent_packages, :bigint, default: 0
   end
 end
