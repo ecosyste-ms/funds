40c3a860195d5858558e3bd9d201e0cb5340f8fa
Andrew Nesbitt <andrewnez@gmail.com>
2024-12-16 11:45:48 +0000

    Add amount column to admin invitations index

--- app/controllers/admin/invitations_controller.rb
+++ app/controllers/admin/invitations_controller.rb
@@ -3,6 +3,7 @@ class Admin::InvitationsController < Admin::ApplicationController
     @invitations = Invitation
       .select('invitations.*, projects.url, projects.name as project_name, funds.name as fund_name, funds.slug as fund_slug')
       .joins(project_allocation: [:project, :fund])
+      .includes(:project_allocation)
       .order(created_at: :desc)
   end
 end
