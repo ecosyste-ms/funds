11e983eac7b5664bc7559f8e63dc849888f62b10
Andrew Nesbitt <andrewnez@gmail.com>
2025-03-24 11:25:01 +0000

    Add methods to calculate completed and in-progress allocations totals

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -744,4 +744,12 @@ class Fund < ApplicationRecord
   def latest_allocation
     allocations.order(created_at: :desc).first
   end
+
+  def completed_allocations_total
+    (allocations.completed.sum(&:complete_payout_total_cents) / 100.0).round(2)
+  end
+
+  def in_progress_allocations_total
+    allocations.not_completed.sum(:total_cents) / 100.0
+  end
 end
